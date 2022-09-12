8c76fef6debe9a801427b62977f5c046e2499321
Abu Hasnat <hasnat.tushar@gmail.com>
2022-09-11 21:17:03 -0500

    Lab 1 final

--- SD-340-W22SD-Labs/Program.cs
+++ SD-340-W22SD-Labs/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿using System.Text;
+
+string WordConcatenate(string wordArg, int numArg)
+{
+    StringBuilder sb = new StringBuilder();
+    for(int i = 0; i < numArg; i++)
+    {
+        sb.Append(wordArg);
+    }
+
+    return sb.ToString();
+}
+
+string testString = "hello world";
+
+stringDelegate newDel = new stringDelegate(WordConcatenate);
+Console.WriteLine(newDel(testString, 5));
+
+newDel = (string wordArg, int numArg) =>
+{
+    string subWord = wordArg.Substring(0, wordArg.Length - numArg);
+    return subWord;
+};
+
+Console.WriteLine(newDel("catastrophe", 4));
+
+delegate string stringDelegate(string wordArg, int numArg);
